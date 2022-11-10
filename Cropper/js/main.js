39faddf95002e9f4c1b7855591773d51c9c8a214
katayama <tattu.0310@gmail.com>
2022-11-10 18:12:47 +0900

    refa

--- Cropper/js/main.js
+++ Cropper/js/main.js
@@ -1,9 +1,10 @@
-var cropperImg = document.getElementById('cropper-img');
-var cropper = new Cropper(cropperImg);
+let cropperImg = document.getElementById('cropper-img');
+let cropper = new Cropper(cropperImg);
 
-document.getElementById('crop-btn').addEventListener('click', function () {
+document.getElementById('crop-btn').addEventListener('click', () => {
     resultImgUrl = cropper.getCroppedCanvas().toDataURL();
-    var result = document.getElementById('result-img');
+    console.log(resultImgUrl, typeof resultImgUrl, "resultImgUrl");
+    let result = document.getElementById('result-img');
     result.src = resultImgUrl;
     alert("切り抜き完了しました。");
 });
